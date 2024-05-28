6c0807ae0bde82dcb9734cb8ec6b46d56857369d
Borista <117987381+FOMUBAD-BORISTA-FONDI@users.noreply.github.com>
2024-05-28 22:07:55 +0100

    react-dom added fully

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import MyComponent from './MyComponent';
 import './App.css';
 
@@ -41,18 +42,80 @@ function App() {
         <button className="btn deselect-btn" onClick={handleDeselectClick}>Deselect</button>
       </div>
       <div className='document'>
-        {pages.map((text, index) => (
-          selected ? (
-            <MyComponent key={index}>
-              <div className='page'>{text}</div>
-            </MyComponent>
+        {pages.map((text, index) => {
+          const pageElement = <div className='page' key={index}>{text}</div>;
+          return selected ? (
+            ReactDOM.createPortal(
+              <MyComponent key={index}>{pageElement}</MyComponent>,
+              document.querySelector('.document')
+            )
           ) : (
-            <div className='page' key={index}>{text}</div>
-          )
-        ))}
+            pageElement
+          );
+        })}
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+// import React, { useState } from 'react';
+// import MyComponent from './MyComponent';
+// import './App.css';
+
+// function App() {
+//   const [selected, setSelected] = useState(false);
+//   const [pages, setPages] = useState([]);
+//   const [inputValue, setInputValue] = useState("");
+
+//   const handleSelectClick = () => {
+//     setSelected(true);
+//   };
+
+//   const handleDeselectClick = () => {
+//     setSelected(false);
+//   };
+
+//   const handleAddPage = () => {
+//     if (inputValue.trim()) {
+//       setPages([...pages, inputValue]);
+//       setInputValue("");
+//     }
+//   };
+
+//   const handleInputChange = (event) => {
+//     setInputValue(event.target.value);
+//   };
+
+//   return (
+//     <div className="App">
+//       <div className="button-container">
+//         <input
+//           type="text"
+//           value={inputValue}
+//           onChange={handleInputChange}
+//           placeholder="Enter page text"
+//           className="text-input"
+//         />
+//         <button className="btn add-btn" onClick={handleAddPage}>Add Page</button>
+//         <button className="btn select-btn" onClick={handleSelectClick}>Select</button>
+//         <button className="btn deselect-btn" onClick={handleDeselectClick}>Deselect</button>
+//       </div>
+//       <div className='document'>
+//         {pages.map((text, index) => (
+//           selected ? (
+//             <MyComponent key={index}>
+//               <div className='page'>{text}</div>
+//             </MyComponent>
+//           ) : (
+//             <div className='page' key={index}>{text}</div>
+//           )
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
